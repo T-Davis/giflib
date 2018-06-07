eabc8da4ad1c8923bde06a24014350de37253550
Trevor Davis <tzendou@gmail.com>
2018-06-07 13:44:10 -0700

    Add gif pojo and repo, and Autowire controller

--- src/main/java/com/trevor/giflib/controller/GifController.java
+++ src/main/java/com/trevor/giflib/controller/GifController.java
@@ -1,12 +1,27 @@
 package com.trevor.giflib.controller;
 
+import com.trevor.giflib.data.GifRepository;
+import com.trevor.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
